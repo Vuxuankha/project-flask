9881b56cf1fa62b2225feaaebdda6708eebc4c58
Vuxuankha <102339314+Vuxuankha@users.noreply.github.com>
2023-06-19 12:26:50 +0000

    kha
    night 19/6

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data analyst',
+    'location': 'Benaluru,India',
+    'salary': 'Rs.10000'
+  },
+  {
+    'id': 2,
+    'title': 'Data scrien',
+    'location': 'Deihl,India',
+    'salary': 'Rs.150000'
+  },
+  {
+    'id': 3,
+    'title': 'App website',
+    'location': 'Remote',
+    'salary': 'Rs.20000'
+  },
+]
 
 
 @app.route('/')
 def hello_world():
-  return "Hello my "
+  return render_template('home.html', job=JOBS, company_name='kha')
 
 
 if __name__ == "__main__":
